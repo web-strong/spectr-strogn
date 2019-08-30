e4c833a70f6c11a55639b92d2d621141810461c7
devjatyjtom <zhekamuntjan@gmail.com>
2019-08-30 15:36:06 +0300

    change images in slider and map, add preloader

--- html/temp/js/main.js
+++ html/temp/js/main.js
@@ -1,26 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    var preloader = $('.preloader'),
+        progressText = $('.preloader .percent'),
+        imagesCount = $('img').length,
+        body = $('body'),
+        percent = Math.floor(100 / imagesCount),
+        progress = 0,
+        loadedImg = 0;
+
+    if (imagesCount > 0) {
+        preloader.css('background', '#fff');
+        body.css('overflow', 'hidden');
+    }
+
+    for (var i = 0; i < imagesCount; i++) {
+        var imageCopy = new Image();
+
+        imageCopy.src = document.images[i].src;
+        imageCopy.onload = imgLoad;
+        imageCopy.onerror = imgLoad;
+    }
+
+    function imgLoad () {
+        progress += percent;
+        loadedImg++;
+
+        if (progress >= 100 || loadedImg == imagesCount) {
+            // progress = 100;
+            preloader.delay(500).fadeOut('slow');
+            body.css('overflow', '');
+        }
+        progressText.text(progress + '%');
+    }
 
     var arrow = '<img src="./img/arrow-right.svg" class="arrow animated delay-1s" data-animation="slideInLeft">',
         rectangleBackground = '<img src="./img/rectangle-horizontal.svg" class="rectangle animated delay-1s" data-animation="slideInRight">',
         text = '<span class="animated delay-1s" data-animation="fadeIn">next</span>',
         btnNext = '<button type="button" class="slick-arrow slick-next ">' + arrow + text + rectangleBackground + '</button>';
 
-    $(".team-slider").slick({
+    $('.team-slider').slick({
         infinite: true,
         slidesToShow: 1,
         dots: true,
         prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
         nextArrow: btnNext,
-        autoplay: true,
-        autoplaySpeed: 2000
+        // autoplay: true,
+        // autoplaySpeed: 2000
     });
-    $(".our-profile_slider").slick({
+    $('.our-profile_slider').slick({
         infinite: false,
         slidesToShow: 3,
         dots: false,
         arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
         responsive: [
             {
                 breakpoint: 991,
@@ -39,25 +72,24 @@ $(document).ready(function() {
             }
         ]
     });
-    $(".show-js").click(function () {
+    $('.show-js').click(function () {
         var item = $(this);
-        if (item.hasClass("active")) {
-            item.removeClass("active")
-        } else {
-            item.addClass("active")
+        if (item.hasClass('active')) {
+            item.removeClass('active');
         }
-        item.next(".hide-info").slideToggle();
+        else {
+            item.addClass('active');
+        }
+        item.next('.hide-info').slideToggle();
     });
-    $(".city").click(function () {
-        $(".city").removeClass("active");
-        $(this).addClass("active");
-    });
+});
+
+$(window).on('load', function () {
 
     var windowWidth = $(window).outerWidth(true);
 
     $(window).scroll(function (e) {
         e.preventDefault();
-
         runAnimation();
     });﻿
 
@@ -67,45 +99,54 @@ $(document).ready(function() {
 
         $('.animated').each(function () {
 
-            var item= $(this),
+            var item = $(this),
                 elemPos = item.offset().top,
                 offsetTop = 0;
 
             if (windowWidth > 1365) {
                 offsetTop = 700;
-            } else if (windowWidth > 767) {
+            }
+            else if (windowWidth > 767) {
                 offsetTop = 600;
-            } else if (windowWidth > 319) {
+            }
+            else if (windowWidth > 319) {
                 offsetTop = 450;
             }
 
             var topOfWindow = $(window).scrollTop() + offsetTop;
 
             if (elemPos < topOfWindow || topOfWindow > $(document).height() - 400) {
-
                 item.addClass(item.data('animation'));
 
                 if (item.hasClass('typing-text') && !item.hasClass('animated-show')) {
-                    animateText(item,1);
+                    animateText(item, 1);
                 }
                 item.addClass('animated-show');
             }
         });
     }
 
-    function animateText(item, index) {
+    function animateText (item, index) {
 
         var text = item.data('text'),
             subText = text.substring(0, index++);
 
-        item.text(subText);
-
-        if (index <= text.length) {
+        if (item.hasClass('delay-2s')) {
             setTimeout(function () {
                 animateText(item, index);
-            }, 100);
+            }, 2000);
+            item.removeClass('delay-2s');
         } else {
-            index = 0;
+            item.text(subText);
+
+            if (index <= text.length) {
+                setTimeout(function () {
+                    animateText(item, index);
+                }, 100);
+            }
+            else {
+                index = 0;
+            }
         }
     }
 });
